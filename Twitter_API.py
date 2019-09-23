3bf4669bdb568144dc26382da4278eb0acbe3335
Tingyi Zhang <55212288+TingyiZhang@users.noreply.github.com>
2019-09-23 13:49:19 -0400

    Add files via upload

---
+++ Twitter_API.py
@@ -0,0 +1,18 @@
+import tweepy
+
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+search_results = api.search(q='keyword',count=100000)
+
+
+for tweet in search_results:
+    print tweet.text
