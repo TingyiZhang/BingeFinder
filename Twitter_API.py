fe1c5d0e4cddafffb51789abd31009a4046d96c2
Tingyi Zhang <55212288+TingyiZhang@users.noreply.github.com>
2019-09-23 13:54:11 -0400

    Update Twitter_API.py

--- Twitter_API.py
+++ Twitter_API.py
@@ -11,7 +11,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-search_results = api.search(q='keyword',count=100000)
+search_results = api.search(q='keyword',count=100)
 
 
 for tweet in search_results:
